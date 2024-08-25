9b01db0ae1f45bc041939336cfe05d39bf7fb740
codePYvibz <vaibhavtheblogger@gmail.com>
2024-08-25 17:14:10 +0530

    API
    . Folder arranged
    . sign and registration backend done(user signup needs to be done)
    . linking of frontend fields to backend need to be done

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Registration = require("./models/registrations.js");
-const Users = require("./models/Users");
+const Users = require("./models/Users.js");
 const path = require("path");
 // const methodOverride = require("method-override"); //For sending POST & DELETE Request
 // const ejsMate = require("ejs-mate");
@@ -18,7 +18,7 @@ main()
 });
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/MOAYUSH');
+  await mongoose.connect('mongodb://127.0.0.1:27017/user');
 };
 
 // Setting up view engine
@@ -34,28 +34,34 @@ app.get("/",(req,res)=>{
     res.send("Hi I am Ministry Of Ayush");
 });
 
-// Setting up API request for user registrations
-app.post('/userlogin', async (req, res) => {
-    try{
-        const {email, password} = req.body;
+// // Setting up API request for user registrations
+// app.post('/userlogin', async (req, res) => {
+//     try{
+//         const {email, password} = req.body;
 
-        // Create a new user 
-        const newUser = new Users({
-            email: email,
-            password: password
-        });
+//         // Create a new user 
+//         const newUser = new Users({
+//             email: email,
+//             password: password
+//         });
 
-        // save the user to the database
-        await newUser.save();
-        res.status(201).send("User registered successfully!");
-    }
-    catch(err){
-        console.error(err);
-        res.status(500).send("Error registering user.");
-    }
-});
+//         // save the user to the database
+//         await newUser.save();
+//         res.status(201).send("User registered successfully!");
+//     }
+//     catch(err){
+//         console.error(err);
+//         res.status(500).send("Error registering user.");
+//     }
+// });
 
 // Setting up the Server
 app.listen(8080, ()=>{
     console.log("Server is listening to port 8080");
 });
+
+// Routes
+const registrationRouter = require('./registrationRoutes.js')
+app.use(registrationRouter);
+const userRouter = require('./userRoutes.js')
+app.use(userRouter);
